670a0d98eefe763275830fccaae4c81f684004ae
sachin kunwar <sachinkunwar284@gmail.com>
2024-10-17 09:53:01 +0545

    first commit

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,25 @@
+
+
+// const AboutUs = () => {
+//   return (
+//     <div>AboutUs</div>
+//   )
+// }
+
+// export default AboutUs
+import React from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
+const AboutUs = () => {
+  return (
+    <>
+      <Hero
+        title={"Learn More About Us | ZeeCare Medical Institute"}
+        imageUrl={"/about.png"}
+      />
+      <Biography imageUrl={"/whoweare.png"} />
+    </>
+  );
+};
+
+export default AboutUs;
---
+++ frontend/src/pages/Appointment.jsx
@@ -0,0 +1,26 @@
+
+
+// const Appointment = () => {
+//   return (
+//     <div>Appointment</div>
+//   )
+// }
+
+// export default Appointment
+import React from "react";
+import Hero from "../components/Hero";
+import AppointmentForm from "../components/AppointmentForm";
+
+const Appointment = () => {
+  return (
+    <>
+      <Hero
+        title={"Schedule Your Appointment | ZeeCare Medical Institute"}
+        imageUrl={"/signin.png"}
+      />
+      <AppointmentForm/>
+    </>
+  );
+};
+
+export default Appointment;
